094a00a0fdd1a96b26498d659fd2b4e23b4687fa
aajai1 <aajai6382@gmail.com>
2025-01-30 20:35:00 +0530

    minor change

--- TestWebsite/src/main/java/com/utils/SourceLink.java
+++ TestWebsite/src/main/java/com/utils/SourceLink.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class SourceLink {
 	
 	public static void source(WebDriver driver) {
-		 String link = "https://testautomationpractice.blogspot.com/";
+		 String sourceLink = "https://testautomationpractice.blogspot.com/";
 		 driver.manage().window().maximize();
 		 
 	}
