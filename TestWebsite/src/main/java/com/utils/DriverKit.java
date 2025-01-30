47746c49cf738e7a17ff934333f4adb41693a37d
aajai1 <aajai6382@gmail.com>
2025-01-30 19:53:39 +0530

    New Project Created to Test the site.

---
+++ TestWebsite/src/main/java/com/utils/DriverKit.java
@@ -0,0 +1,11 @@
+package com.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverKit {
+	public static WebDriver driverTool() {
+		return new ChromeDriver();
+	}
+
+}
